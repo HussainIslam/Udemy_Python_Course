2038456d2d46fb0e6728ec935ee89ec6f249fbd1
hussain <m.hussainul.islam@gmail.com>
2019-08-22 18:18:02 -0400

    S6 done

--- S6_The_Basics_Processing_User_Input/string_format.py
+++ S6_The_Basics_Processing_User_Input/string_format.py
@@ -1,5 +1,7 @@
 user_input = input('Enter your name: ')
 message = 'Hello %s from first string format!' % user_input
 message2 = f'Hello {user_input} from second string format!'
+message3 = 'Hello {} from third string format!'.format(user_input)
 print(message)
 print(message2)
+print(message3)
