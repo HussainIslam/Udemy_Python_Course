ce8c37664e520416908e2dc845a5411d27daa64a
hussain <m.hussainul.islam@gmail.com>
2019-08-22 06:25:37 -0400

    s3 done

---
+++ S3_The_Basics_Data_Types/dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+student_grades = { "Marry": 9.1, "Sim": 8.8, "John": 7.5 }
+mysum = sum(student_grades.values())
+length = len(student_grades)
+print(mysum / length)
---
+++ S3_The_Basics_Data_Types/grades.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+student_grades = [ 9.1, 8.8, 7.5 ]
+mysum = sum(student_grades)
+length = len(student_grades)
+print(mysum / length)
